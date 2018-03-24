7d4941ca6110e8a3e0d2ff967214cc302c9df5c4
matheus trajano <matrajano12@gmail.com>
2018-03-24 04:18:19 -0300

    Finalizando centralização e edição da tela recordes

--- src/br/com/novaroma/showDoMilhaoModoDev/apresentacao/TelaRecorde.java
+++ src/br/com/novaroma/showDoMilhaoModoDev/apresentacao/TelaRecorde.java
@@ -8,6 +8,8 @@ import javax.swing.border.EmptyBorder;
 
 import br.com.novaroma.showDoMilhaoModoDev.entidades.Jogador;
 import br.com.novaroma.showDoMilhaoModoDev.negocio.JogadorController;
+import java.awt.Color;
+import java.awt.Font;
 
 public class TelaRecorde extends JFrame {
 
@@ -17,20 +19,28 @@ public class TelaRecorde extends JFrame {
 
     public TelaRecorde() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 909, 433);
         contentPane = new JPanel();
+        contentPane.setBackground(new Color(250, 235, 215));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
+        this.setLocationRelativeTo(null);
 
         JScrollPane scrollPane = new JScrollPane();
         scrollPane.setBounds(10, 246, 424, -233);
         contentPane.add(scrollPane);
 
         lblNewLabel = new JLabel("New label");
-        lblNewLabel.setBounds(10, 11, 414, 239);
+        lblNewLabel.setFont(new Font("Agency FB", Font.PLAIN, 22));
+        lblNewLabel.setBounds(239, 87, 414, 296);
 
         contentPane.add(lblNewLabel);
+        
+        JLabel lblRecordes = new JLabel("Recordes");
+        lblRecordes.setFont(new Font("Agency FB", Font.PLAIN, 30));
+        lblRecordes.setBounds(394, 34, 105, 42);
+        contentPane.add(lblRecordes);
 
         iniciarRecordes();
     }
